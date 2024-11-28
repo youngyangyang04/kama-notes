401d5548513968cbac6311734bd669321bf07901
charon2121 <2833121040@qq.com>
2024-11-28 17:28:13 +0800

    test husky with lint-staged

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import './App.css'
 
 function App() {
-  return (
-    <div>
-      App
-    </div>
-  )
+  const test = 'test...'
+
+  console.log(test)
+
+  return <div>App</div>
 }
 
 export default App
