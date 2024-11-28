a4cb49fd6415744972f29f0f71b865e1e49834d0
charon2121 <2833121040@qq.com>
2024-11-28 19:17:40 +0800

    chore: 配置 antd + antd icons 组件库

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,11 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { ConfigProvider } from 'antd'
+import zhCN from 'antd/locale/zh_CN'
 
-createRoot(document.getElementById('root')!).render(<App />)
+createRoot(document.getElementById('root')!).render(
+  <ConfigProvider locale={zhCN}>
+    <App />
+  </ConfigProvider>,
+)
