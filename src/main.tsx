508524d01bb5fa107148e9f78a248beb1de289f8
charon2121 <2833121040@qq.com>
2024-12-11 10:18:11 +0800

    chore: 安装配置 react-router-dom + redux

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import './index.css'
 import App from './App.tsx'
 import { ConfigProvider } from 'antd'
 import zhCN from 'antd/locale/zh_CN'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <ConfigProvider locale={zhCN}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </ConfigProvider>,
 )
