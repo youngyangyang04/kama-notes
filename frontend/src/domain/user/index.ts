01c0061e09fd65c9da5c86e46d38d646d54181c8
charon2121 <2833121040@qq.com>
2024-12-12 17:46:23 +0800

    test

--- frontend/src/domain/user/index.ts
+++ frontend/src/domain/user/index.ts
@@ -1,8 +1,8 @@
-import type { UserEntity, UserState, Banned } from './types/types.ts'
+import type { UserEntity, UserState } from './types/types.ts'
 import { Gender, Admin } from './types/types.ts'
 import LoginModal from './components/LoginModal.tsx'
 import UserAvatarMenu from './components/UserAvatarMenu.tsx'
 
-export type { UserEntity, UserState, Banned }
+export type { UserEntity, UserState }
 export { Gender, Admin }
 export { LoginModal, UserAvatarMenu }
